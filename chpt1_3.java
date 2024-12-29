6f37ab1a1df023df3c8cf98b3c9c562660b9bc42
TGSpring <TylerGSpring@gmail.com>
2024-12-29 12:50:05 -0500

    Chapter 1 question 3

---
+++ chpt1_3.java
@@ -0,0 +1,16 @@
+/**
+ * Tyler Spring
+ * 12/29/2024
+ * chapter 1 problem 3 
+ */
+
+public class chpt1_3 {
+    public static void main(String[] args) {
+       int num = 12;
+
+       for (int i = 1; i <= 12; i++) {
+        //Print the product of the number and i
+        System.out.println(num + " * " + i + " = " + (num * 1));
+       }
+    }
+}
