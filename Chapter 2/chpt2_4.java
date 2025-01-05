65223f20a572001de58e578aacce00915b8f760c
TGSpring <TylerGSpring@gmail.com>
2025-01-05 12:31:29 -0500

    2.4

---
+++ Chapter 2/chpt2_4.java
@@ -0,0 +1,16 @@
+/**
+ * Tyler Spring
+ * 1/5/2025
+ * 2.4
+ * perfect squares
+ */
+public class chpt2_4 {
+    
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i * i);
+        }
+    }
+}
