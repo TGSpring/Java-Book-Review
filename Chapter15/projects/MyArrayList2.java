a17df7b35504b2556f17f8cc2fa1f245109fec0b
TGSpring <TylerGSpring@gmail.com>
2025-11-19 13:40:53 -0500

    Base arrayList class done. Next will be adding in subList. Happy NVIDIA earnings day

--- Chapter15/projects/MyArrayList2.java
+++ Chapter15/projects/MyArrayList2.java
@@ -66,9 +66,7 @@ public class MyArrayList2<E> {
 
     // Adds element at specific index.
     public void add(int index, E element) {
-        if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException("Index: " + index);
-        }
+        checkAddIndex(index);
         ensureCapacity(size + 1);
         for (int i = size; i > index; i--) {
             elementData[i] = elementData[i - 1];
@@ -106,10 +104,70 @@ public class MyArrayList2<E> {
         return false;
     }
 
+    // private remove method to prevent removing objects.
+    private E removeAtIndex(int index) {
+        return remove(index);
+    }
+
     // Index bounds check helper.
     private void checkIndex(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
     }
+
+    // clear method.
+    // When we build subList, calling this on a subList must translate to removing that entire range form the parent list.
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elementData[i] = null; // Free references for GC.
+        }
+        size = 0;
+    }
+
+    // indexOf method.
+    // This provides the backbone for search, contains, and for debugging subList later.
+    public int indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (o == null ? elementData[i] == null : o.equals(elementData[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // contains method.
+    // logically just indexOf >= 0.
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+    //checkAddIndex method.
+    // Mirrors java's real ArrayList.
+    private void checkAddIndex(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    // toString.
+    // Self explanatory.
+    // This is the only instance in my entire repo I will use StringBuilder.
+    // I do not care if its slightly cheating.
+    public String toString() {
+        if (size == 0) {
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < size; i++) {
+            sb.append(elementData[i]);
+            if (i < size - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
 }
